f573c81a0a800fbb65144d3beda895c6f54afb72
Tyler Wade <vaxonreal@gmail.com>
2025-03-05 11:05:48 -0500

    Added routes to delete specific api calls or all api calls in a service.

--- backend/src/main/java/net/tylerwade/backend/apicall/APICallController.java
+++ backend/src/main/java/net/tylerwade/backend/apicall/APICallController.java
@@ -12,7 +12,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@RestController @RequestMapping("/api/apicalls")
+@RestController
+@RequestMapping("/api/apicalls")
 public class APICallController {
 
     private final APICallRepository apiCallRepository;
@@ -90,20 +91,90 @@ public class APICallController {
         // Filter by path
         if (path != null && !path.isEmpty()) {
             apiCalls = apiCalls.stream().filter(apiCall -> apiCall
-                    .getPath().toLowerCase()
-                    .contains(path.toLowerCase()))
+                            .getPath().toLowerCase()
+                            .contains(path.toLowerCase()))
                     .collect(Collectors.toList());
         }
 
         // Filter by method
         if (method != null && !method.isEmpty()) {
             apiCalls = apiCalls.stream().filter(apiCall -> apiCall
-                    .getMethod().toLowerCase()
-                    .contains(method.toLowerCase()))
+                            .getMethod().toLowerCase()
+                            .contains(method.toLowerCase()))
                     .collect(Collectors.toList());
         }
 
         return new ResponseEntity<>(apiCalls, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteApiCall(@PathVariable("id") Long id,
+                                           @RequestHeader("serviceId") String serviceId,
+                                           @CookieValue("auth_token") String authToken
+    ) {
+        // Validate User
+        User user = userService.getUserFromAuthToken(authToken);
+        if (user == null) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Verify service exists
+        Optional<Service> serviceOptional = serviceRepository.findById(serviceId);
+        if (serviceOptional.isEmpty()) {
+            return new ResponseEntity<>("Service not found", HttpStatus.NOT_FOUND);
+        }
+
+        Service service = serviceOptional.get();
+
+        // Verify owner of service
+        if (!service.getUserId().equals(user.getId())) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Verify API Call exists and is in service
+        Optional<APICall> apiCallOptional = apiCallRepository.findById(id);
+        if (apiCallOptional.isEmpty()) {
+            return new ResponseEntity<>("API call not found", HttpStatus.NOT_FOUND);
+        }
+
+        // Verify API Call is in service
+        if (!apiCallOptional.get().getServiceId().equals(serviceId)) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Delete the api call
+        apiCallRepository.delete(apiCallOptional.get());
+
+        return new ResponseEntity<>("API Call deleted.", HttpStatus.OK);
+    }
+
+    @DeleteMapping({"/", ""})
+    public ResponseEntity<?> deleteAllApiCallsInService(
+            @RequestHeader String serviceId,
+            @CookieValue("auth_token") String authToken
+    ) {
+        // Validate user
+        User user = userService.getUserFromAuthToken(authToken);
+        if (user == null) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        Optional<Service> serviceOptional = serviceRepository.findById(serviceId);
+        // Verify service exists
+        if (serviceOptional.isEmpty()) {
+            return new ResponseEntity<>("Service not found", HttpStatus.NOT_FOUND);
+        }
+
+        Service service = serviceOptional.get();
+        // Verify owner of service
+        if (!service.getUserId().equals(user.getId())) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Delete all api calls in service
+        apiCallRepository.deleteAllByServiceId(serviceId);
+
+        return new ResponseEntity<>("All API Calls in service deleted.", HttpStatus.OK);
     }
 
 }
--- backend/src/main/java/net/tylerwade/backend/apicall/APICallRepository.java
+++ backend/src/main/java/net/tylerwade/backend/apicall/APICallRepository.java
@@ -1,6 +1,8 @@
 package net.tylerwade.backend.apicall;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ public interface APICallRepository extends JpaRepository<APICall, Long> {
 
 
     List<APICall> findAllByServiceId(String serviceId);
+
+    @Modifying
+    @Transactional
+    void deleteAllByServiceId(String serviceId);
 }
